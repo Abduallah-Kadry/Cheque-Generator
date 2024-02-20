16df97ec3fc95e850bccfc9235bd9a9a4100fc8f
abduallah kadry <abduallahkadry2001@gmail.com>
2024-02-20 22:17:04 +0200

    Cheque Generator 80% complete

---
+++ src/main/java/com/tajmisr/TajMisrApplication.java
@@ -0,0 +1,13 @@
+package com.tajmisr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajMisrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TajMisrApplication.class, args);
+    }
+
+}
